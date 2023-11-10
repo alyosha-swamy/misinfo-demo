954a9b5c0a4e3968974de6e5042281e3a1026bdf
Raghav Ravishankar <113712354+alyosha-swamy@users.noreply.github.com>
2023-11-10 19:20:08 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -20,28 +20,34 @@ for key, value in default_session_state.items():
     if key not in st.session_state:
         st.session_state[key] = value
 
-# Function to evaluate misinformation
 def evaluate_misinformation(statement: str, api_key: str) -> Dict[str, object]:
     prompt = f'Evaluate the following statement: "{statement}". \
 Provide an analysis of the statement and rate its truthfulness on a scale from 0 to 30. \
 Consider the following metrics: Verifiability, Sourcing, Logical Consistency, Relevance, Completeness, Bias, Clarity, Safety, User Understanding, Fluency. \
 Do not begin your response with a number. First write your analysis, then write a vertical bar "|", \
 then finally state your overall truthfulness score and another vertical bar "|", \
-followed by your internal scores in the format Verifiability:1/3|Sourcing:2/3|...'
+followed by your internal scores in the format Verifiability:1/3|Sourcing:2/3|...Provide you explanation first without considering the metrics, use them only for your score'
+
     openai.api_key = api_key
 
     response = openai.ChatCompletion.create(
         model="gpt-4-0613",
         messages=[{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": prompt}],
-        max_tokens=300
+        max_tokens=300,
+        n=2  # Get top 2 responses
     )
-    out_of = None
-    output_text = response['choices'][0]['message']['content'].strip()
-    analysis, score, internal_scores_str = output_text.split("|", 2)
 
-    # Split the output to get the analysis, the score, and the internal_scores
+    # Extract the top 2 responses
+    analysis_1 = response['choices'][0]['message']['content'].strip()
+    analysis_2 = response['choices'][1]['message']['content'].strip()
 
-    
+    # Split the first response to get the analysis, the score, and the internal_scores
+    parts = analysis_1.split("|", 2)
+    if len(parts) == 3:
+        analysis_1, score, internal_scores_str = parts
+    else:
+        st.error("The analysis doesn't contain enough information.")
+
     internal_scores = {}
     for item in internal_scores_str.split("|"):
         if ":" in item:
@@ -49,12 +55,11 @@ followed by your internal scores in the format Verifiability:1/3|Sourcing:2/3|..
             internal_scores[metric.strip()] = value.strip()
 
     return {
-        'analysis': analysis.strip(),
+        'analysis_1': analysis_1.strip(),
+        'analysis_2': analysis_2.strip(),
         'score': score,
-        'out_of': out_of,
         'internal_scores': internal_scores
     }
-
 
 def save_feedback(feedback_data):
     # Load existing data
@@ -90,51 +95,30 @@ if st.session_state.evaluate_statement:
     if statement:
         result = evaluate_misinformation(statement, api_key)
 
-        st.write(f"Analysis: {result['analysis']}")
+        # Create two columns
+        col1, col2 = st.columns(2)
+
+        # Write the analyses in each column
+        col1.write(f"Analysis 1: {result['analysis_1']}")
+        col2.write(f"Analysis 2: {result['analysis_2']}")
+
+        # Radio button group for user to select one of the analyses
+        selected_analysis = st.radio(
+            "Select the analysis you agree with:",
+            ('Analysis 1', 'Analysis 2')
+        )
+
         st.write(f"Internal Scores: {result['internal_scores']}")
-        st.write(f"Truthfulness Score: {result['score']} out of {result['out_of']}")
+        st.write(f"Truthfulness Score: {result['score']}")
 
-    st.write("Do you agree with the analysis?")
-    if 'agree' not in st.session_state:
-        st.session_state.agree = False
-    if 'disagree' not in st.session_state:
-        st.session_state.disagree = False
+        # Rest of your code...
 
-    if st.button("👍", on_click=lambda: setattr(st.session_state, 'agree', True)):
-        st.session_state.disagree = False
-    if st.button("👎", on_click=lambda: setattr(st.session_state, 'disagree', True)):
-        st.session_state.agree = False
-
-    if st.session_state.agree:
-        st.session_state.feedback_score = "Agree"
-    if st.session_state.disagree:
-        st.session_state.feedback_score = "Disagree"
-
-    
-    with st.form(key='feedback_form'):
-        questions = [
-            'Was the response clear and understandable?', 
-            'Did the response answer your query?', 
-            'Was the information provided accurate?', 
-            'Was the response appropriate?'
-        ]
-        responses = {question: st.selectbox(question, ['Select an option', 'Yes', 'No']) for question in questions}
-        additional_feedback = st.text_area("Please provide additional feedback:")
-        submit_button = st.form_submit_button(label='Submit Feedback')
-
-        if submit_button:
-            if all(response != 'Select an option' for response in responses.values()):
-                st.session_state['form_feedback_questions'] = responses
-                st.session_state['user_feedback'] = additional_feedback
-            else:
-                st.warning('Please respond to all feedback questions.')
-
-    # Check if all necessary data is loaded in
-    if 'feedback_score' in st.session_state and 'form_feedback_questions' in st.session_state and 'user_feedback' in st.session_state:
+        # Add selected_analysis to feedback_data
         feedback_data = {
             "datetime": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             "user_prompt": statement,
-            "gpt_analysis": result['analysis'],
+            "selected_analysis": selected_analysis,
+            "gpt_analysis": result['analysis_1'] if selected_analysis == 'Analysis 1' else result['analysis_2'],
             "gpt_score": result['score'],
             "gpt_internal_scores": result['internal_scores'],
             "feedback_score": st.session_state.feedback_score,
